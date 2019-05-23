13fafadb357b31d490cbfb253d40fdf8ce6f30f1
Ernie Bryant <ernie.bryant@gmail.com>
2019-05-22 23:33:53 -0400

    Added cordova packages and images

--- src/app/record-button/record-button.component.ts
+++ src/app/record-button/record-button.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
-declare var Replay:any;
+//declare var Replay:any;
 
 @Component({
   selector: 'app-record-button',
@@ -32,38 +32,68 @@ export class RecordButtonComponent implements OnInit {
 
   startRecording = (enableMic) => {
     if (this.plt.is("ios")) {
-      alert("is ios!");
-      window.cordova.plugins.Replay.startRecording(enableMic,
-        function() {
-            this.isRecording = true;
-          }, function(err) {
-            console.log(err);
-          }
-      );
+      this.startIosRecording();
+    }
+    else if (this.plt.is("android")){
+      this.startAndroidRecording();
     }
     else {
-      alert("is not ios!");
+      this.startWebRecording();
     }
     
   };
 
   stopRecording = () => {
     if (this.plt.is("ios")) {
-      alert("is ios!");
-
-      window.cordova.plugins.Replay.stopRecording(
-        function() {
-          this.isRecording = false;
-            console.log('ok!');
-        }, function(err) {
-          console.log(err);
-        }
-      );
+      this.stopIosRecording();
+    }
+    else if (this.plt.is("android")){
+      this.stopAndroidRecording();
     }
     else {
-      alert("is not ios!");
+      this.stopWebRecording();
     }
       
   }
 
+  startIosRecording = () => {
+    alert("is ios!");
+      // window.cordova.plugins.Replay.startRecording(enableMic,
+      //   function() {
+      //       this.isRecording = true;
+      //     }, function(err) {
+      //       console.log(err);
+      //     }
+      // );
+  }
+  stopIosRecording = () => {
+    alert("is ios!");
+
+      // window.cordova.plugins.Replay.stopRecording(
+      //   function() {
+      //     this.isRecording = false;
+      //       console.log('ok!');
+      //   }, function(err) {
+      //     console.log(err);
+      //   }
+      // );
+  }
+
+  startAndroidRecording = () => {
+    alert("is Android startRecording");
+    //Use ionic-screen-recorder
+  }
+  stopAndroidRecording = () => {
+    alert("is Android stopRecording");
+    //Use ionic-screen-recorder
+  }
+
+  startWebRecording = () => {
+    alert("is Web startRecording");
+    // use cordova-replay
+  }
+  stopWebRecording = () => {
+    alert("is Web stopRecording");
+    // use cordova-replay
+  }
 }
